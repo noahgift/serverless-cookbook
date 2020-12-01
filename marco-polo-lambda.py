25d91832083e6750d5fa3acbefc2003334b33aae
Noah Gift <noah.gift@gmail.com>
2020-12-01 12:42:20 -0500

    Update marco-polo-lambda.py

--- marco-polo-lambda.py
+++ marco-polo-lambda.py
@@ -1,3 +1,11 @@
+"""
+To invoke from CLI:
+
+aws lambda invoke --function-name MarcoPolo9000 --payload '{"name": "Marco" }' out.txt | less out.txt
+
+"""
+
+
 def lambda_handler(event, context):
     if event["name"] == "Marco":
         return "Polo"
