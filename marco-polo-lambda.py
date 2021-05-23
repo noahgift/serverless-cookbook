72f2fe13ca2b30cea9988981e22f3e4e50a97463
Noah Gift <noah.gift@gmail.com>
2021-05-23 07:28:48 -0400

    Update marco-polo-lambda.py

--- marco-polo-lambda.py
+++ marco-polo-lambda.py
@@ -1,7 +1,15 @@
 """
 To invoke from CLI:
 
+AWS CLI v1
 aws lambda invoke --function-name MarcoPolo9000 --payload '{"name": "Marco" }' out.txt | less out.txt
+
+AWS CLI v2
+aws lambda invoke \
+	 --cli-binary-format raw-in-base64-out \
+	--function-name marcopython \
+	--payload '{"name": "Marco"}' \
+	response.json
 
 """
 
